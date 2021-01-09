07c97e38c9e49775c87cf896a981616d6ec07217
AbserAri <abserari@gmail.com>
2021-01-09 20:43:58 +0800

    test error on github action

--- pkg/artwork/search_test.go
+++ pkg/artwork/search_test.go
@@ -2,6 +2,7 @@ package artwork
 
 import (
 	"context"
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -41,6 +42,7 @@ func TestSearchArtwork(t *testing.T) {
 	r18 := R18result.Artworks()
 	for _, i := range r18 {
 		var found bool
+		log.Println(i.Tags)
 		for _, v := range i.Tags {
 			if v != "R-18" && v != "R-18G" {
 				continue
